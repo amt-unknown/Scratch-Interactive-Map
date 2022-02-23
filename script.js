803a7c1841593083ebce5eae899ac05ebed9e613
Andrew Thompson <amtunknown@Andrews-MacBook-Pro.local>
2022-02-22 17:28:36 -0800

    Final commit: a few features could be added, such as, marker removal

--- script.js
+++ script.js
@@ -1,50 +1,83 @@
-async function initializeMap(){
+const userMap = {
      //Grab user location
-    pos = await new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject);
-    })
+    userPosition: [],
+    userLocalMap:  {},
+    userMarker: {},
+    locations: [],
+    locationMarkers: {},
 
-    const myMap = L.map('map', {
-        center: [pos.coords.latitude, pos.coords.longitude],
-        zoom: 12,
-    });
-
-    // Add OpenStreetMap tiles:
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        minZoom: '15',
-    }).addTo(myMap)
+    initializeMap() {
+        this.userLocalMap = L.map('map', {center: this.userPosition, zoom: 11,});
     
-    const marker = L.marker([pos.coords.latitude, pos.coords.longitude])
-    marker.addTo(myMap).bindPopup('<p1><b>Your Location</b></p1>').openPopup()
+        // Add OpenStreetMap tiles:
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            minZoom: '10',
+        }).addTo(this.userLocalMap)
+    
+        //Add user location marker
+        const marker = L.marker(this.userPosition)
+        marker.addTo(this.userLocalMap).bindPopup('<p1><b>Your are here</b></p1>').openPopup()
+    },
+
+    addBusinesLocations(){
+        this.locations.forEach((location) => {
+            userMap.locationMarkers = L.marker([location.lat, location.long])
+            userMap.locationMarkers.addTo(userMap.userLocalMap).bindPopup(`<p1><b>${location.name}</b></p1>`).openPopup()
+        })
+    },
 
 
+}
+
+//Get user location 
+async function getUserPosition(){
+    let pos = await new Promise((resolve, reject) => {navigator.geolocation.getCurrentPosition(resolve, reject);});
+    console.log(pos.coords)
     return [pos.coords.latitude, pos.coords.longitude]
 }
 
-let user = initializeMap()
+//Wait for page load
+window.onload = async () => {
+    let coords = await getUserPosition()
+    userMap.userPosition = coords
+    userMap.initializeMap()
+}
 
+//Fetch Foursquare data via API
+async function getFoursquareData(category){
+    const options = {
+        method: 'GET',
+        headers: {
+        Accept: 'application/json',
+        Authorization: 'fsq3eRAeSflBeplmnMkHnAdQqcQJS8bvOIGgJXchlCZjE6g='
+        }
+    }
+    let lat = userMap.userPosition[0]
+    let long = userMap.userPosition[1]
 
+    
+    let response = await fetch(`https://api.foursquare.com/v3/places/search?ll=${lat}%2C${long}&radius=3000&query=${category}&limit=5`, options)
+    let data = await response.json()
+    let localBusinesses = data.results
 
+    return localBusinesses
+}
 
-// Create map:   
 
 let locationButtons = document.querySelectorAll(".locationButton")
-
 locationButtons.forEach((location) => { 
-    location.addEventListener('click', () => {
-        console.log(location.textContent)
+    location.addEventListener('click', async () => {
+        let category = location.textContent;
+        let selectionData = await getFoursquareData(category)
+        
+        selectionData.forEach((location) => {
+            userMap.locations.push({name: location.name, lat: location.geocodes.main.latitude, long: location.geocodes.main.longitude})
+        })
+        userMap.addBusinesLocations()
+ 
     })
 })
-
-    
-   
-    
-    //Events for local coffee, restaurant, hotel, and market location buttons
-    
-    //Locate 5 nearest locations of interest near user location
-    
-    //Events for posted locations on map(research needed)
 
 
 
